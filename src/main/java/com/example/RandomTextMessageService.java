5d2a244b1265ccf58d3b3e280eba9d3b4a112965
Jakub Szostak <jakub.szostakk@gmail.com>
2022-12-07 16:47:04 +0100

    podpunkt 8

---
+++ src/main/java/com/example/RandomTextMessageService.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RandomTextMessageService implements MessageService{
+    String[] messages = {"wiadomosc1", "wiadomosc2", "wiadomosc3", "wiadomosc4", "wiadomosc5", "wiadomosc6", "wiadomosc7", "wiadomosc8", "wiadomosc9", "wiadomosc10" };
+
+    @Override
+    public String getMessage() {
+        Random rand = new Random();
+        int n = rand.nextInt();
+        return messages[n];
+    }
+}
