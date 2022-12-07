044c8f7f03ca70b1f0a92353b85fc87eaea89402
Jakub Szostak <jakub.szostakk@gmail.com>
2022-12-07 17:08:33 +0100

    podpunkt 13, 14, 15

--- src/main/java/com/example/RandomTextMessageService.java
+++ src/main/java/com/example/RandomTextMessageService.java
@@ -1,8 +1,11 @@
 package com.example;
 
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.Random;
 
+@Component("messageService")
 public class RandomTextMessageService implements MessageService{
     String[] messages = {"wiadomosc1", "wiadomosc2", "wiadomosc3", "wiadomosc4", "wiadomosc5", "wiadomosc6", "wiadomosc7", "wiadomosc8", "wiadomosc9", "wiadomosc10" };
 
--- src/test/java/com/example/MyNameMessageServiceTest.java
+++ src/test/java/com/example/MyNameMessageServiceTest.java
@@ -1,10 +1,12 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.stereotype.Component;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MyNameMessageServiceTest {
+@Component("messageService")
+public class MyNameMessageServiceTest {
 
     @Test
     public void shouldReturnMyNameAsMessage() {
