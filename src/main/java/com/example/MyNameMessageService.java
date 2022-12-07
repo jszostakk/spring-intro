7a6aff75b800d69ccf0e4f6c2324a553e6b02164
Jakub Szostak <jakub.szostakk@gmail.com>
2022-12-07 16:37:52 +0100

    podpunkt 6

--- src/main/java/com/example/MyNameMessageService.java
+++ src/main/java/com/example/MyNameMessageService.java
@@ -1,4 +1,8 @@
 package com.example;
 
 public class MyNameMessageService implements MessageService {
+    public String getMessage(){
+        String message = "Jakub Szostak";
+        return message;
+    }
 }
