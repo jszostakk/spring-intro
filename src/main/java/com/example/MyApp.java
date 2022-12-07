29ef9d4ccba025d5b318dc18bcf8ff4441829b1d
Jakub Szostak <jakub.szostakk@gmail.com>
2022-12-07 17:35:21 +0100

    podpunkt 1 Spring Dependency Injection

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -8,12 +8,21 @@ public class MyApp {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         MessageService messageService = applicationContext.getBean("messageService", MessageService.class);
-        MessageService messageService1 = applicationContext.getBean("messageService", MessageService.class);
-
-        System.out.println(messageService.hashCode());
-        System.out.println(messageService1.hashCode());
+        MessageService messageService1 = applicationContext.getBean("messageService1", MessageService.class);
+        MessageService messageService2 = applicationContext.getBean("messageService1", MessageService.class);
 
         System.out.println(messageService.getMessage());
+        System.out.println(messageService.hashCode());
+
+        System.out.println(messageService1.getMessage());
+        System.out.println(messageService1.hashCode());
+
+        System.out.println(messageService2.getMessage());
+        System.out.println(messageService2.hashCode());
+
+
+
+
 
         applicationContext.close();
     }
--- src/main/java/com/example/RandomTextMessageService.java
+++ src/main/java/com/example/RandomTextMessageService.java
@@ -2,10 +2,9 @@ package com.example;
 
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.Random;
 
-@Component("messageService")
+@Component("messageService1")
 public class RandomTextMessageService implements MessageService{
     String[] messages = {"wiadomosc1", "wiadomosc2", "wiadomosc3", "wiadomosc4", "wiadomosc5", "wiadomosc6", "wiadomosc7", "wiadomosc8", "wiadomosc9", "wiadomosc10" };
 
